aefc818e5f7cd2bf37e766e9ecbd09d5955f2496
Eduardo Freitas <edusf8700@gmail.com>
2020-07-14 01:30:00 -0300

    Adicionando cors e exibindo categorias na listagem de transações

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -6,6 +6,7 @@ import {
   JoinColumn,
   CreateDateColumn,
 } from 'typeorm';
+
 import Category from './Category';
 
 @Entity('transactions')
@@ -22,7 +23,7 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
